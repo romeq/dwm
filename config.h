7cb47564c21de93ca6eb76d01d3302c8655940c7
romeq <me@romeq.fi>
2022-02-16 18:40:04 +0200

    top bar back, align audiokeys and add screenshot key

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMonoMedium Nerd Font Mono:size=12", "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#1d2021";
@@ -60,12 +60,14 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *screenshotcmd[] = { "sh", "/home/rmq/scripts/ss.sh", NULL };
 static const char *raisevolumecmd[] = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%", NULL };
 static const char *lowervolumecmd[] = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%", NULL };
 static const char *mutevolumecmd[] = { "pactl", "set-sink-mute", "@DEFAULT_SINK@", "toggle", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+    { MODKEY,                       XK_s,      spawn,          {.v = screenshotcmd } },
 	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
@@ -87,9 +89,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-    { 0,                         XF86XK_AudioRaiseVolume, spawn, {.v = raisevolumecmd } },
-    { 0,                         XF86XK_AudioLowerVolume, spawn, {.v = lowervolumecmd } },
-    { 0,                         XF86XK_AudioMute, spawn,      {.v = mutevolumecmd } },
+    { 0,                            XF86XK_AudioRaiseVolume, spawn, {.v = raisevolumecmd } },
+    { 0,                            XF86XK_AudioLowerVolume, spawn, {.v = lowervolumecmd } },
+    { 0,                            XF86XK_AudioMute,        spawn, {.v = mutevolumecmd } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
